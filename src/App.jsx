7823fe1f379aaaeb4c8b66e7ab9699a524aabc9a
Saurav Singh <sauravsingh98866@gmail.com>
2026-05-01 17:08:22 +0530

    fix: footer import case

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import AppRoute from './routes/AppRoute';
 import "./index.css";
 import Navbar from './components/Navbar';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 function App() {
 
